7ed0c8338bd85f503d45251d3510fa235fe66139
Maxime <maximaxandre@gmail.com>
2025-10-22 17:14:58 +0200

    Création TP1 devine mon nombre

--- TP1_guessMyNumber_ANDRE/src/tp1_guessmynumber_andre/TP1_guessMyNumber_ANDRE.java
+++ TP1_guessMyNumber_ANDRE/src/tp1_guessmynumber_andre/TP1_guessMyNumber_ANDRE.java
@@ -4,6 +4,8 @@
  */
 package tp1_guessmynumber_andre;
 
+import java.util.Scanner;
+
 /**
  *
  * @author maxim
@@ -14,7 +16,8 @@ public class TP1_guessMyNumber_ANDRE {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-    }
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Entrez le premier entier : ");
+        int a = sc.nextInt();
     
-}
+
