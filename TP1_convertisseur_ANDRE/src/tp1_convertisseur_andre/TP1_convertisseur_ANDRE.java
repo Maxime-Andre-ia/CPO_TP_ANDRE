024a5ebf25d85bcbbcd7b3742273d6deaa3488f6
Maxime <maximaxandre@gmail.com>
2025-10-24 10:51:27 +0200

    Création TP1 convertisseur Temp le 24/10

--- TP1_convertisseur_ANDRE/src/tp1_convertisseur_andre/TP1_convertisseur_ANDRE.java
+++ TP1_convertisseur_ANDRE/src/tp1_convertisseur_andre/TP1_convertisseur_ANDRE.java
@@ -6,16 +6,46 @@ package tp1_convertisseur_andre;
 
 import java.util.Scanner;
 
-
 /**
  *
  * @author maxim
  */
 public class TP1_convertisseur_ANDRE {
 
-    /**
-     * @param args the command line arguments
-     */
+    public static double CelciusVersKelvin(double tCelcius) {
+        double tKelvin = tCelcius + 273.15;
+        return tKelvin;
+    }
+
+    public static double KelvinVersCelcius(double tKelvin) {
+        double tCelcius = tKelvin - 273.15;
+        return tCelcius;
+    }
+
+    public static double FarenheitVersCelcius(double tFarenheit) {
+        double tCelcius = (tFarenheit - 32) * 5 / 9;
+        return tCelcius;
+    }
+
+    public static double CelciusVersFarenheit(double tCelcius) {
+        double tFarenheit = (tCelcius * 9 / 5) + 32;
+        return tFarenheit;
+    }
+
+    public static double KelvinVersFarenheit(double tKelvin) {
+        double tFarenheit = (tKelvin - 273.15) * 9 / 5 + 32;
+        return tFarenheit;
+    }
+
+    public static double FarenheitVersKelvin(double tFarenheit) {
+        double tKelvin = (tFarenheit - 32) * 5 / 9 + 273.15;
+        return tKelvin;
+    }
+        
+        
+        /**
+         * @param args the command line arguments
+         */
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
@@ -26,10 +56,8 @@ public class TP1_convertisseur_ANDRE {
         // Formule : T(K) = T(°C) + 273.15
         double tKelvin = tCelsius + 273.15;
 
-        // 3. Affichage du résultat
+        // Affichage du résultat
         System.out.println("Temperature en degres Celsius : " + tCelsius);
         System.out.println("Temperature correspondante en degres Kelvin : " + tKelvin);
     }
 }
-
-
