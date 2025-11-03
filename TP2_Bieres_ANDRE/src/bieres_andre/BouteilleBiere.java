0e27d77651436f5bf595ea775b2c9f98830f8855
Maxime <maximaxandre@gmail.com>
2025-11-03 15:19:43 +0100

    Création TP2 03/11/2025 Partie Bouteille Bière

--- TP2_Bieres_ANDRE/src/bieres_andre/BouteilleBiere.java
+++ TP2_Bieres_ANDRE/src/bieres_andre/BouteilleBiere.java
@@ -10,13 +10,11 @@ package bieres_andre;
  */
 public class BouteilleBiere {
 
-    
     String nom;
     float degreAlcool;
     String brasserie;
     boolean ouverte;
 
-    
     public void lireEtiquette() {
         System.out.println("Bouteille de " + nom + " (" + degreAlcool + " degrés)");
         System.out.println("Brasserie : " + brasserie);
@@ -27,5 +25,27 @@ public class BouteilleBiere {
         degreAlcool = unDegre;
         brasserie = uneBrasserie;
         ouverte = false; // par défaut, la bouteille est fermée
+    }
+
+    public boolean Decapsuler() {
+        if (!ouverte) {          // si la bouteille était fermée
+            ouverte = true;      // on l'ouvre
+            return true;         // succès
+        } else {
+            System.out.println("erreur : biere deja ouverte");
+            return false;        // échec
+        }
+    }
+
+    @Override
+    public String toString() {
+        String chaine_a_retourner;
+        chaine_a_retourner = nom + " (" + degreAlcool + " degres Ouverte ? ";
+        if (ouverte == true) {
+            chaine_a_retourner += "oui";
+        } else {
+            chaine_a_retourner += "non";
+        }
+        return chaine_a_retourner;
     }
 }
