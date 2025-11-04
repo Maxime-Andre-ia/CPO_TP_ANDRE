71704fff0faf9c1db57734425beefdb6aa08b987
Maxime <maximaxandre@gmail.com>
2025-11-04 11:18:11 +0100

    Création  exo 3 TP2

---
+++ TP2_manip_ANDRE/src/tp2_manip_andre/Moussaka.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp2_manip_andre;
+
+/**
+ *
+ * @author maxim
+ */
+public class Moussaka {
+
+    int nbCalories;
+
+    public Moussaka(int calories) {
+        nbCalories = calories;
+    }
+
+    public Moussaka() {
+        nbCalories = 0;
+    }
+}
