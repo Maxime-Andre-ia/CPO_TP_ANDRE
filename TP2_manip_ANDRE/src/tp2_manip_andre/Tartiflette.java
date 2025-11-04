df7461186ea1d6eb167c215084efaf3289be9ed8
Maxime <maximaxandre@gmail.com>
2025-11-04 11:17:58 +0100

    Création  exo 3 TP2

---
+++ TP2_manip_ANDRE/src/tp2_manip_andre/Tartiflette.java
@@ -0,0 +1,18 @@
+package tp2_manip_andre;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author maxim
+ */
+public class Tartiflette {
+   int nbCalories;   // attribut ajouté en Q2
+
+    public Tartiflette(int calories) {
+        nbCalories = calories;
+    }
+}
