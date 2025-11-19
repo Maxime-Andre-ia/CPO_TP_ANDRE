74d67918d0a56fac6b717e1e90c062547cb2fe16
Maxime <maximaxandre@gmail.com>
2025-11-19 11:19:44 +0100

    Création  exo 4 TP2 Q25

---
+++ TP3_Heroic_Fantasy_ANDRE/src/tp3_heroic_fantasy_andre/Guerrier.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp3_heroic_fantasy_andre;
+
+/**
+ *
+ * @author maxim
+ */
+public class Guerrier extends Personnage {
+
+    // Q23 : propre au guerrier
+    private boolean aCheval;    // true = à cheval, false = à pied
+
+    // Q24 : initialiser à cheval / à pied dans le constructeur
+    public Guerrier(String nom, int vie, boolean estACheval) {
+        super(nom, vie);        // initialise nom et niveauVie dans Personnage
+        aCheval = estACheval;
+    }
+
+    // Q25 : setter pour cette propriété
+    public void setACheval(boolean estACheval) {
+        aCheval = estACheval;
+    }
+}
