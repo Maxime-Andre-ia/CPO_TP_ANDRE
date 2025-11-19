4e9aff1f4751a53e574489404d62947e026060c3
Maxime <maximaxandre@gmail.com>
2025-11-19 11:19:59 +0100

    Création  exo 4 TP2 Q25

---
+++ TP3_Heroic_Fantasy_ANDRE/src/tp3_heroic_fantasy_andre/Personnage.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp3_heroic_fantasy_andre;
+
+/**
+ *
+ * @author maxim
+ */
+
+
+public abstract class Personnage {
+
+    private final String nom;
+    private final int niveauVie;
+
+    public Personnage(String nomPersonnage, int vie) {
+        nom = nomPersonnage;
+        niveauVie = vie;
+    }
+
+    public int getNiveauVie() {
+        return niveauVie;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    @Override
+    public String toString() {
+        return "Personnage \"" + nom + "\" (vie = " + niveauVie + ")";
+    }
+}
+
+
