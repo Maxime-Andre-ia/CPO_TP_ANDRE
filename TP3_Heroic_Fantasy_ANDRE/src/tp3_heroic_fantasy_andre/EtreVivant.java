69df0b5a0ae6b29faaa90d993ff1f7bf03e56ffb
Maxime <maximaxandre@gmail.com>
2025-11-25 23:53:21 +0100

    Création TP3

---
+++ TP3_Heroic_Fantasy_ANDRE/src/tp3_heroic_fantasy_andre/EtreVivant.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package tp3_heroic_fantasy_andre;
+
+public interface EtreVivant {
+    
+    // Q48 : Déclaration des méthodes (sans le code, car c'est une interface)
+    void seFatiguer();
+    boolean estVivant();
+    void estAttaque(int points);
+}
