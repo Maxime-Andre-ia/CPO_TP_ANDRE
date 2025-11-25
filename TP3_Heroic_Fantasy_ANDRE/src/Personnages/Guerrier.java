15a52d3b3c18b89b5326cdfa10ebf14f51807bbd
Maxime <maximaxandre@gmail.com>
2025-11-25 22:49:14 +0100

    Création  exo 4 TP2

--- TP3_Heroic_Fantasy_ANDRE/src/Personnages/Guerrier.java
+++ TP3_Heroic_Fantasy_ANDRE/src/Personnages/Guerrier.java
@@ -4,25 +4,47 @@
  */
 package Personnages;
 
-import Personnages.Personnage;
-
 /**
  *
  * @author maxim
  */
 public class Guerrier extends Personnage {
 
-    // Q23 : propre au guerrier
+    // Q44 : Compteur statique (propre à la classe, pas à l'objet)
+    private static int nbGuerriers = 0;
+
+    // Q23 : Attribut d'instance
     private boolean aCheval;    // true = à cheval, false = à pied
 
-    // Q24 : initialiser à cheval / à pied dans le constructeur
+    // Q24 & Q45 : UNIQUE Constructeur fusionné
     public Guerrier(String nom, int vie, boolean estACheval) {
-        super(nom, vie);        // initialise nom et niveauVie dans Personnage
-        aCheval = estACheval;
+        super(nom, vie);        // Initialise Personnage (et augmente le compteur total)
+        this.aCheval = estACheval;
+        
+        // On augmente le compteur spécifique aux guerriers
+        nbGuerriers++;
     }
 
-    // Q25 : setter pour cette propriété
+    // Q25 : Setter
     public void setACheval(boolean estACheval) {
-        aCheval = estACheval;
+        this.aCheval = estACheval;
+    }
+
+    // Getter pour l'attribut aCheval (utile pour les combats plus tard)
+    public boolean getACheval() {
+        return aCheval;
+    }
+
+    // Méthode statique pour lire le nombre de guerriers
+    public static int getNbGuerriers() {
+        return nbGuerriers;
+    }
+    
+    // Q46 : Destructeur (si tu veux l'ajouter maintenant)
+    @Override
+    protected void finalize() {
+        if (nbGuerriers > 0) {
+            nbGuerriers--;
+        }
     }
 }
