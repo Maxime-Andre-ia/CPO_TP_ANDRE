cf7d4671418416677469eae5e107e7cb991e32f0
Maxime <maximaxandre@gmail.com>
2025-11-25 21:44:18 +0100

    Création  exo 4 TP2

--- TP3_Heroic_Fantasy_ANDRE/src/tp3_heroic_fantasy_andre/TP3_Heroic_Fantasy_ANDRE.java
+++ TP3_Heroic_Fantasy_ANDRE/src/tp3_heroic_fantasy_andre/TP3_Heroic_Fantasy_ANDRE.java
@@ -4,6 +4,10 @@
  */
 package tp3_heroic_fantasy_andre;
 
+import Personnages.Magicien;
+import Personnages.Guerrier;
+import Personnages.Personnage;
+import Personnages.*;
 import Armes.Epee;
 import Armes.Baton;
 import java.util.ArrayList;
@@ -40,5 +44,35 @@ public class TP3_Heroic_Fantasy_ANDRE {
         for (int i = 0; i < armes.size(); i++) {
             System.out.println(armes.get(i));
         }
+        // --- 2.3 Création des personnages ---
+
+        // Q26 : Création des 2 magiciens [cite: 53, 54]
+        // Paramètres : Nom, PV, Confirmé (true) / Novice (false)
+        Magicien mag1 = new Magicien("Gandalf", 65, true);
+        Magicien mag2 = new Magicien("Garcimore", 44, false);
+
+        // Q27 : Création des 2 guerriers [cite: 55]
+        // Paramètres : Nom, PV, À cheval (true) / À pied (false)
+        // Attention : "Conan" est à pied, donc false pour 'aCheval'
+        Guerrier guer1 = new Guerrier("Conan", 78, false);
+        Guerrier guer2 = new Guerrier("Lannister", 45, true);
+
+        // Q28 : Tableau dynamique pour les personnages 
+        // IMPORTANT : On utilise le type <Personnage> pour stocker à la fois
+        // des Magiciens et des Guerriers (Polymorphisme).
+        ArrayList<Personnage> personnages = new ArrayList<>();
+
+        personnages.add(mag1);
+        personnages.add(mag2);
+        personnages.add(guer1);
+        personnages.add(guer2);
+
+        // Affichage des caractéristiques [cite: 57]
+        System.out.println("\n--- Caractéristiques des Personnages ---");
+        for (int i = 0; i < personnages.size(); i++) {
+            // Cela appelle la méthode toString() de la classe Personnage
+            System.out.println(personnages.get(i));
+        }
     }
+
 }
