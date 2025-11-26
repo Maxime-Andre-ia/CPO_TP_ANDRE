aead7f7170cbc8a1caa8b2487c9c1f9210c8fe5a
Maxime <maximaxandre@gmail.com>
2025-11-26 13:56:57 +0100

    Création TP3

--- TP3_Heroic_Fantasy_ANDRE/src/Personnages/Magicien.java
+++ TP3_Heroic_Fantasy_ANDRE/src/Personnages/Magicien.java
@@ -12,13 +12,10 @@ package Personnages;
 
 public class Magicien extends Personnage {
 
-    // Q44 : Compteur statique
     private static int nbMagiciens = 0;
 
-    // Q23 : Attribut d'instance
-    private boolean confirme;   // true = confirmé, false = novice
+    private boolean confirme;   
 
-    // Q24 & Q45 : Constructeur
     public Magicien(String nom, int vie, boolean estConfirme) {
         super(nom, vie);
         this.confirme = estConfirme;
@@ -26,22 +23,18 @@ public class Magicien extends Personnage {
         nbMagiciens++;
     }
 
-    // Q25 : Setter
     public void setConfirme(boolean estConfirme) {
         this.confirme = estConfirme;
     }
 
-    // Getter (utile pour les combats)
     public boolean getConfirme() {
         return confirme;
     }
 
-    // Méthode statique pour lire le nombre de magiciens
     public static int getNbMagiciens() {
         return nbMagiciens;
     }
     
-    // Q46 : Destructeur
     @Override
     protected void finalize() {
         if (nbMagiciens > 0) {
@@ -49,29 +42,23 @@ public class Magicien extends Personnage {
         }
     }
 
-    // Q53 : L'attaque du Magicien (VERSION UNIQUE ET CORRECTE)
     @Override
     public void attaquer(Personnage cible) {
         if (getArmeEnMain() != null) {
             
-            // 1. Récupérer le niveau d'attaque de l'arme
             int degats = getArmeEnMain().getNiveauAttaque();
 
-            // 2. Si c'est un Bâton, on multiplie par l'âge
             if (getArmeEnMain() instanceof Armes.Baton baton) {
                 int age = baton.getAge();
                 degats *= age;
             }
 
-            // 3. Si le magicien est confirmé, dégâts divisés par 2
             if (this.confirme) {
                 degats /= 2;
             }
 
-            // 4. Le magicien se fatigue
             seFatiguer();
 
-            // 5. On applique les dégâts
             System.out.println(getNom() + " attaque " + cible.getNom() + " avec " + getArmeEnMain().getNom() + " !");
             cible.estAttaque(degats);
 
