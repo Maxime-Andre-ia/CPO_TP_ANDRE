31478e3d1c9262a2d638cedaa8d8afa94676d17f
Maxime <maximaxandre@gmail.com>
2025-11-26 13:56:50 +0100

    Création TP3

--- TP3_Heroic_Fantasy_ANDRE/src/Personnages/Personnage.java
+++ TP3_Heroic_Fantasy_ANDRE/src/Personnages/Personnage.java
@@ -5,7 +5,6 @@ import java.util.ArrayList;
 
 public abstract class Personnage implements EtreVivant {
 
-    // Q43 : Compteur statique partagé par TOUS les objets Personnage
     private static int nbPersonnages = 0; 
 
     private final ArrayList<Arme> inventaire = new ArrayList<>();
@@ -17,16 +16,13 @@ public abstract class Personnage implements EtreVivant {
         this.nom = nomPersonnage;
         this.niveauVie = vie;
         
-        // Q45 : On augmente le compteur à chaque création de personnage
         nbPersonnages++; 
     }
 
-    // Q43 : Getter statique pour lire le nombre total de personnages
     public static int getNbPersonnages() {
         return nbPersonnages;
     }
 
-    // Getters classiques
     public int getNiveauVie() {
         return niveauVie;
     }
@@ -35,7 +31,6 @@ public abstract class Personnage implements EtreVivant {
         return nom;
     }
 
-    // Q32 : Ajouter une arme à l'inventaire (max 5)
     public void ajouterArme(Arme a) {
         if (inventaire.size() < 5) {
             inventaire.add(a);
@@ -44,12 +39,10 @@ public abstract class Personnage implements EtreVivant {
         }
     }
 
-    // Q34 : Getter pour savoir quelle arme est portée
     public Arme getArmeEnMain() {
         return armeEnMain;
     }
 
-    // Q35 : Choisir une arme dans l'inventaire
     public void equiperArme(String nomArme) {
         for (Arme a : inventaire) {
             if (a.getNom().equals(nomArme)) {
@@ -61,7 +54,6 @@ public abstract class Personnage implements EtreVivant {
         System.out.println("Arme " + nomArme + " non trouvee dans l'inventaire de " + nom);
     }
 
-    // Q36 : Méthode toString complète
     @Override
     public String toString() {
         String info = "Personnage \"" + nom + "\" (vie = " + niveauVie + ")";
@@ -73,25 +65,21 @@ public abstract class Personnage implements EtreVivant {
         return info;
     }
 
-    // Q50 : La fatigue fait perdre 10 points
     @Override
     public void seFatiguer() {
         niveauVie -= 10;
         System.out.println(nom + " se fatigue (-10 PV).");
     }
 
-    // Q51 : Est vivant si vie > 0
     @Override
     public boolean estVivant() {
         return niveauVie > 0;
     }
 
-    // Q52 : Subir une attaque (baisse les PV)
     @Override
     public void estAttaque(int points) {
         niveauVie -= points;
         System.out.println(nom + " est attaque et perd " + points + " points !");
     }
-    // Q53 : Méthode abstraite pour l'attaque (à définir dans Guerrier et Magicien)
     public abstract void attaquer(Personnage cible);
 }
